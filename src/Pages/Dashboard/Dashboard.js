12727fb60de88d2d0e44ac0947e9cf26b7820558
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-11-15 18:52:08 +0600

    dashboard edited

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,20 +1,34 @@
 import { Container } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import PageBanner from '../../Shared/PageBanner/PageBanner';
 import DashboardLink from './DashboardLinks/DashboardLink';
 import './Dashboard.css';
+import UseAuth from '../../hooks/UseAuth';
 
 const Dashboard = () => {
     const pageName = "Dashboard";
+    const { user } = UseAuth();
+    const [adminStatus, setAdminStatus] = useState(false);
     const pages = [
         "My Orders", "Manage Products", "Add Product", "Manage Orders", "Add Review", "Make Admin", "Payment"
     ]
     const links = [
-         "/myorders", "/manageproducts", "/addproduct", "/manageorders", "/addreview",
-         "makeadmin", "/payment"
+        "/myorders", "/manageproducts", "/addproduct", "/manageorders", "/addreview",
+        "makeadmin", "/payment"
     ]
+
+    //check admin status
+    useEffect(() => {
+        const url = `https://serene-beyond-56458.herokuapp.com/users/${user?.email}`;
+        fetch(url, {
+            method: 'get'
+        }).then(res => res.json())
+            .then(data => setAdminStatus(data?.isAdmin))
+
+    }, [user]);
+
     return (
         <div>
             <Header></Header>
@@ -26,34 +40,42 @@ const Dashboard = () => {
                             Controll Your Account
                         </h2>
                         <div className="dashboard-links">
-                            <DashboardLink
-                            page={pages[0]}
-                            link={links[0]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[1]}
-                            link={links[1]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[2]}
-                            link={links[2]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[3]}
-                            link={links[3]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[4]}
-                            link={links[4]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[5]}
-                            link={links[5]}
-                            ></DashboardLink>
-                            <DashboardLink
-                            page={pages[6]}
-                            link={links[6]}
-                            ></DashboardLink>
+                            {
+                             !adminStatus &&  <>
+                                <DashboardLink
+                                    page={pages[0]}
+                                    link={links[0]}
+                                ></DashboardLink>
+                                <DashboardLink
+                                    page={pages[4]}
+                                    link={links[4]}
+                                ></DashboardLink>
+                                <DashboardLink
+                                    page={pages[6]}
+                                    link={links[6]}
+                                ></DashboardLink>
+                            </>
+                            }
+                            {
+                                adminStatus && <>
+                                    <DashboardLink
+                                        page={pages[3]}
+                                        link={links[3]}
+                                    ></DashboardLink>
+                                    <DashboardLink
+                                        page={pages[1]}
+                                        link={links[1]}
+                                    ></DashboardLink>
+                                    <DashboardLink
+                                        page={pages[2]}
+                                        link={links[2]}
+                                    ></DashboardLink>
+                                    <DashboardLink
+                                        page={pages[5]}
+                                        link={links[5]}
+                                    ></DashboardLink>
+                                </>
+                            }
                         </div>
                     </div>
                 </Container>
