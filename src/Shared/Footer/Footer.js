ec4a9ee7c001aa90b7429213cda262cb32ba7f86
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-04-21 02:25:51 +0600

    footer updated

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,46 +1,95 @@
 import React from 'react';
-import { faFacebookF, faGooglePlusG, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Footer.css';
-import { Container} from '@mui/material';
+import { Box, Container, FormControl, Grid, Input, InputAdornment } from '@mui/material';
+import { Link } from 'react-router-dom';
+import EmailIcon from '@mui/icons-material/Email';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="footer-top">
-                <div className="social-icons">
-                    <div className="social-btn"><FontAwesomeIcon icon={faFacebookF} /><span style={{ marginLeft: '5px' }}>Facebook</span></div>
-                    <div className="social-btn"><FontAwesomeIcon icon={faTwitter} /><span style={{ marginLeft: '5px' }}>Twitter</span></div>
-                    <div className="social-btn"><FontAwesomeIcon icon={faYoutube} /><span style={{ marginLeft: '5px' }}>Youtube</span></div>
-                    <div className="social-btn"><FontAwesomeIcon icon={faGooglePlusG} /><span style={{ marginLeft: '5px' }}>Google +</span></div>
-                    <div className="social-btn"><FontAwesomeIcon icon={faInstagram} /><span style={{ marginLeft: '5px' }}>Instagram</span></div>
-                </div>
-            </div>
-            <div className="footer-bottom">
-                <Container>
-                    <div className="footer-bottom-container">
-                        <div className="footer-cols">
-                            <h4>Our Office</h4>
-                            <address>
-                                175 SW 7th St #2009,<br /> Miami, Fl 33130,<br />United States
+        <footer style={{ padding: '100px 0px 30px 0px', backgroundImage: 'linear-gradient(to right, var(--primaryColor) , var(--secondaryColor))', borderTopLeftRadius: '150px' }}>
+            <Container>
+                <Grid container spacing={6}>
+                    <Grid item xs={12} sm={12} md={3}>
+                        <Box>
+                            <h3 style={{ color: '#fff', fontSize: '26px' }} className='footer-logo'>CAM TECH</h3>
+                            <p style={{ color: '#fff', fontSize: '16px', lineHeight: '1.4' }}>
+                                Best camera shop in online. Buy top quality camera from camtech. We are best in the market in the term of keeping promises. We will deliver you the best product timely!
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={3}>
+                        <Box>
+                            <h3 style={{ color: '#fff', fontSize: '26px' }}>
+                                OFFICE
+                            </h3>
+                            <address style={{ fontSize: '16px', color: '#fff' }}>
+                                ITPL Road<br />
+                                Whitefield, New York<br />
+                                USA
                             </address>
-                        </div>
-                        <div className="footer-cols">
-                            <h4>Contact Us</h4>
-                            <address>
-                                Phone: 01234567893<br />
-                                Email: camtech@camtech.com
-                            </address>
-                        </div>
-                        <div className="footer-cols">
-                            <h4>News Letter</h4>
-                            <form>
-                                <input placeholder="Your Email" className="footer-email" type="email" /><button className="brand-btn">Subscribe</button>
-                            </form>
-                        </div>
-                    </div>
-                </Container>
-            </div>
+                            <p style={{ fontSize: '16px', color: '#fff', textDecoration: 'underline' }}>
+                                camtech@camtech.com
+                            </p>
+                            <p style={{ fontSize: '16px', color: '#fff' }}>
+                                +99808784653
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={3}>
+                        <Box>
+                            <h3 style={{ color: '#fff', fontSize: '26px' }}>
+                                LINKS
+                            </h3>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <Link className='footer-links' to='/'>Home</Link>
+                                <Link className='footer-links' to='/explore'>Explore</Link>
+                                <Link className='footer-links' to='/'>About</Link>
+                                <Link className='footer-links' to='/'>Contact</Link>
+                                <Link className='footer-links' to='/'>Review</Link>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={3}>
+                        <Box>
+                            <h3 style={{ color: '#fff', fontSize: '26px' }}>
+                                NEWS LETTER
+                            </h3>
+                            <Box>
+                                <FormControl variant="standard">
+                                    <Input
+                                    sx={{color: '#fff'}}
+                                    placeholder="Enter your email id"
+                                        id="input-with-icon-adornment"
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                <EmailIcon sx={{color: '#fff'}} />
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                            </Box>
+                            <Box className='footer-icon-box'>
+                                    <Box className='footer-icon-container'><FacebookOutlinedIcon /></Box>
+                                    <Box className='footer-icon-container'><TwitterIcon /></Box>
+                                    <Box className='footer-icon-container'><WhatsAppIcon /></Box>
+                                    <Box className='footer-icon-container'><PinterestIcon /></Box>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Box>
+                    <Box sx={{padding: '40px 0px' }}>
+                        <Box sx={{ height: '2px', background: '#8f94a7' }}></Box>
+                    </Box>
+                    <Box>
+                        <p style={{color: '#fff', textAlign: 'center'}}>Camtect Limited &copy; 2021 - All Rights Reserved.</p>
+                    </Box>
+                </Box>
+            </Container>
         </footer>
     );
 };
